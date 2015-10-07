f143e3027d299a55aee9ffdb303d8b2878695fb2
timkeane <tkeane@doitt.nyc.gov>
2015-10-07 15:43:32 -0400

    unit tests

--- src/test/webapp/js/nyc/app.js
+++ src/test/webapp/js/nyc/app.js
@@ -108,7 +108,6 @@ QUnit.test('drillDownLink (click first time)', function(assert){
 		infowin.remove();
 		done();
 	}, 1000);
-	
 });
 
 QUnit.test('drillDownLink (click again)', function(assert){
@@ -154,19 +153,19 @@ QUnit.test('drillDownLink (click again)', function(assert){
 		infowin.remove();
 		done();
 	}, 1000);
-
 });
 
-/*
 QUnit.test('drillDown (precint view)', function(assert){
-	assert.expect(6);
+	assert.expect(7);
 			
+	var done = assert.async();
+
 	var updateView = nyc.App.prototype.updateView;
 	nyc.App.prototype.updateView = function(){
 		assert.ok(true);
 	};
 
-	var infowin = $('<div class="cartodb-popup v2"><div class="cartodb-popup-content"><a class="crime-count"></a><ul class="crime-count-type" style="display:none"><li></li></ul></div></div>');
+	var infowin = $('<div class="cartodb-popup v2"><div class="cartodb-popup-content"><a class="crime-count"></a><ul class="crime-count-type" style="display:none"></ul></div></div>');
 	$('body').append(infowin);
 	
 	var btn = $('div.cartodb-popup.v2 .cartodb-popup-content .crime-count');
@@ -183,19 +182,77 @@ QUnit.test('drillDown (precint view)', function(assert){
 		dateRange: new nyc.EventHandling(),
 		crimeDrillDown: this.MOCK_DAO
 	});	
+	app.panPopup = function(args){
+		assert.ok(true);
+	};
 	app.filters = function(){
-		return {filterValues: 'mockFilterValues'};
+		return {filterValues: {filterName: {field: 'value'}}};
 	};
 	
 	app.drillDown({pct: '1'});
 	
-	assert.equal(this.MOCK_DAO.filters, 'mockFilterValues');
-	assert.equal(this.MOCK_DAO.filters.pct, '1');
+	assert.deepEqual(this.MOCK_DAO.filters, {filterName: {field: 'value'}, pct: {pct: '1'}});
 	
 	$('ul.crime-count-type li').each(function(i, li){
 		assert.equal($(li).html(), rows[i].crime_count + ' ' + app.crimeTypePlurals[rows[i].type]);
 	});
 	
-	infowin.remove();
+	setTimeout(function(){
+		assert.equal($('ul.crime-count-type').css('display'), 'block');
+		nyc.App.prototype.updateView = updateView;
+		infowin.remove();
+		done();
+	}, 1000);
 });
-*/
+
+QUnit.test('drillDown (location view)', function(assert){
+	assert.expect(7);
+			
+	var done = assert.async();
+
+	var updateView = nyc.App.prototype.updateView;
+	nyc.App.prototype.updateView = function(){
+		assert.ok(true);
+	};
+
+	var infowin = $('<div class="cartodb-popup v2"><div class="cartodb-popup-content"><a class="crime-count"></a><ul class="crime-count-type" style="display:none"></ul></div></div>');
+	$('body').append(infowin);
+	
+	var btn = $('div.cartodb-popup.v2 .cartodb-popup-content .crime-count');
+
+	var rows = [{type: 'a',  crime_count: 5}, {type: 'b',  crime_count: 2}, {type: 'c',  crime_count: 1}];
+	this.MOCK_DAO.resultData = {rows: rows};
+	
+	var app = new nyc.App({
+		viewSwitcher: this.MOCK_VIEW_SWITCHER,
+		locate: new nyc.EventHandling(),
+		controls: new nyc.EventHandling(),
+		mapType: new nyc.EventHandling(),
+		crimeType: new nyc.EventHandling(), 
+		dateRange: new nyc.EventHandling(),
+		crimeDrillDown: this.MOCK_DAO
+	});	
+	app.panPopup = function(args){
+		assert.ok(true);
+	};
+	app.filters = function(){
+		return {filterValues: {filterName: {field: 'value'}}};
+	};
+	
+	app.drillDown({x: '1', y: '2'});
+	
+	assert.deepEqual(this.MOCK_DAO.filters, {filterName: {field: 'value'}, location: {x: '1', y: '2'}});
+	
+	$('ul.crime-count-type li').each(function(i, li){
+		assert.equal($(li).html(), rows[i].crime_count + ' ' + app.crimeTypePlurals[rows[i].type]);
+	});
+	
+	setTimeout(function(){
+		assert.equal($('ul.crime-count-type').css('display'), 'block');
+		nyc.App.prototype.updateView = updateView;
+		infowin.remove();
+		done();
+	}, 1000);
+
+});
+
